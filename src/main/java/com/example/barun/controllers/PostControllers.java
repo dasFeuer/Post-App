7fd7cbfaf86b28d95bac7add546c7613d76f9b14
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2025-03-19 08:54:28 +0545

    Create post needs to be implemented

--- src/main/java/com/example/barun/controllers/PostControllers.java
+++ src/main/java/com/example/barun/controllers/PostControllers.java
@@ -1,9 +1,14 @@
 package com.example.barun.controllers;
 
+import com.example.barun.entities.postEntities.Post;
+import com.example.barun.entities.userEntities.User;
+import com.example.barun.repositories.UserRepository;
 import com.example.barun.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/posts")
@@ -12,6 +17,8 @@ public class PostControllers {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserRepository userRepository;
 
 
 }
--- src/main/java/com/example/barun/services/PostService.java
+++ src/main/java/com/example/barun/services/PostService.java
@@ -1,9 +1,15 @@
 package com.example.barun.services;
 
+import com.example.barun.entities.postEntities.Post;
+import com.example.barun.entities.userEntities.User;
 import com.example.barun.repositories.PostRepository;
 import com.example.barun.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -14,6 +20,18 @@ public class PostService {
     @Autowired
     private UserRepository userRepository;
 
+
+    public Optional<Post> getBlogById(Long id){
+        return postRepository.findById(id);
+    }
+
+    public List<Post> getAllBlogs(){
+        return postRepository.findAll();
+    }
+
+    public void deleteBlogById(Long id){
+        postRepository.deleteById(id);
+    }
 
 
 }
